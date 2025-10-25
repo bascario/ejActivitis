9eb03f6de9e89acf2cd2883232a24de3aec87a71
Ruben <ruben.jaramillo@istvidanueva.edu.ec>
2025-10-25 11:55:17 -0500

    version agenda

--- app/src/main/java/com/example/activitys/Activity2.kt
+++ app/src/main/java/com/example/activitys/Activity2.kt
@@ -1,20 +1,26 @@
 package com.example.activitys
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity // Importa AppCompatActivity
+import androidx.activity.ComponentActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+import android.widget.ListView
 
-class Activity2 : AppCompatActivity() {
+class Activity2 : ComponentActivity()  {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_2)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val listaView = findViewById<ListView>(R.id.listaView)
+        val botonVolver = findViewById<Button>(R.id.btnVolver)
+        val adaptador = ArrayAdapter(this, android.R.layout.simple_list_item_1, MainActivity.listaAsistencias)
+        listaView.adapter = adaptador
+        botonVolver.setOnClickListener {
+            finish()
         }
     }
 }
--- app/src/main/java/com/example/activitys/MainActivity.kt
+++ app/src/main/java/com/example/activitys/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.activitys
 
 import android.os.Bundle
+import android.content.Intent
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -14,34 +18,32 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.activitys.ui.theme.ActivitysTheme
 
 class MainActivity : ComponentActivity() {
+    companion object{
+        val listaAsistencias = mutableListOf<String>()
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            ActivitysTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
+        setContentView(R.layout.activity_main) // llamamos al layout que hemos creado
+        val txtNombre = findViewById<TextView>(R.id.txtNombre)
+        val btnRegistrar = findViewById<Button>(R.id.btnIrSegunda)
+        val btnVerLista = findViewById<Button>(R.id.btnLista)
+btnRegistrar.setOnClickListener {
+    val nombre = txtNombre.text.toString().trim()
+    if (nombre.isNotEmpty()){
+        listaAsistencias.add(nombre)
+        Toast.makeText(this,"asistencia registrada: $nombre", Toast.LENGTH_SHORT).show()
+        txtNombre.text = ""
+    }else{
+        Toast.makeText(this,"Por favor ingrese un nombre", Toast.LENGTH_SHORT).show()
     }
 }
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+btnVerLista.setOnClickListener {
+    val intent = Intent(this,Activity2::class.java)
+    startActivity(intent)
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ActivitysTheme {
-        Greeting("Android")
+
+
     }
 }
